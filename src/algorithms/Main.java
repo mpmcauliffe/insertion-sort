cd4e03b72342c69c82d09f00fd808506649e146b
mpmcauliffe <mcauliffe99@gmail.com>
2019-04-19 00:11:18 -0600

    algorithm and annotations complete

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -18,12 +18,42 @@ public class Main {
      * ALGORITHM
      **/
         /**
+         *      this algorithm assumes that the first index is sorted then works to the left to expand the
+         *      sorted partition
+         *
          *      OUTER LOOP
+         *          firstUnsortedIndex is always the index to the immediate right of the sorted partition
+         *          this element is always compared with portion of the sorted array then inserted accordingly
          **/
         for(int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
 
+            /**
+             *      newElement is the element associated with firstUnsortedIndex
+             *      as the sorted partition grows and firstUnsortedIndex moves right - i moves left back through
+             *          the sorted partition of the array as a way to compare the sorted elements
+             *          to firstUnsortedIndex
+             **/
+            int newElement =  intArray[firstUnsortedIndex];
+            int i;
+
+            /**
+             *      INNER LOOP
+             *          when the requirements are met it means that the firstUnsortedIndex < last element of the
+             *              sorted partition
+             *          each iteration of the loop compares newElement to the decrementing index of the sorted
+             *              partition until newElement > the element at the current index - the loop terminates
+             **/
+            for(i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--){
+                intArray[i] = intArray[i - 1];
+            }
+
+            /**
+             *      at this point newElement has found its position and is assigned to the sorted partition
+             **/
+            intArray[i] = newElement;
         }
     /****/
+
 
         //  for printing only
         for (int num: intArray) {
