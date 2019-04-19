ee9e15f1b6aa4202f59d2a6c1d550c07d1e34b86
mpmcauliffe <mcauliffe99@gmail.com>
2019-04-18 21:48:41 -0600

    update

---
+++ src/algorithms/Main.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /**  initialize unsorted array with assigned elements **/
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        //  for printing only
+        for (int num: intArray) {
+            System.out.print(num + "  ");
+        }
+        System.out.print("\n");
+
+
+    /**
+     * ALGORITHM
+     **/
+        /**
+         *      OUTER LOOP
+         **/
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+
+        }
+    /****/
+
+        //  for printing only
+        for (int num: intArray) {
+            System.out.print(num + "  ");
+        }
+    }
+}
